3b81f4719909bbac5b385ab59aa60d07f785e595
fallout0128 <68479312+fallout0128@users.noreply.github.com>
2020-09-27 18:37:41 +0300

    save

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'dist/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+        'DB_DIGITAL': JSON.stringify(process.env.DB_DIGITAL || 'off')
+      }
+    })
+  ]
+};
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  watch: true,
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }
+});
